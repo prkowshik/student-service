aae6993d6affac0bd79024f6876a43a82950c2ae
prkowshik <133478002+prkowshik@users.noreply.github.com>
2024-03-30 21:37:16 +0530

    Client side load balancing

---
+++ src/main/java/com/codezen/app/config/AddressClientLoadBalConfig.java
@@ -0,0 +1,19 @@
+package com.codezen.app.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+
+import feign.Feign;
+
+@LoadBalancerClient(value = "address-service")
+public class AddressClientLoadBalConfig {
+	
+	@LoadBalanced
+	@Bean
+	public Feign.Builder feignBuilder(){
+		return Feign.builder();
+	}
+	
+
+}
