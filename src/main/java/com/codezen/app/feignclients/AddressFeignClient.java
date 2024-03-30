bc78b1991193cbb6573028aa9a9eab617a5110c2
prkowshik <133478002+prkowshik@users.noreply.github.com>
2024-03-30 20:45:08 +0530

    feignclient with eureka

--- src/main/java/com/codezen/app/feignclients/AddressFeignClient.java
+++ src/main/java/com/codezen/app/feignclients/AddressFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.codezen.app.model.Address;
 
-@FeignClient(url = "${address.base.url}", value = "address-feign-client", path = "/api/address/")
+@FeignClient(value = "address-service", path = "/api/address/")
 public interface AddressFeignClient {
 	
 	@GetMapping("/getById/{id}")
